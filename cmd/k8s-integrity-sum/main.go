252aac44425d4f013bd56ba871c01ee60f6ac2fd
Siarhei Shliayonkin <sshliayonkin@scnsoft.com>
2023-02-27 14:28:41 +0300

    bee2 integration test in main; not for merge

--- cmd/k8s-integrity-sum/main.go
+++ cmd/k8s-integrity-sum/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"flag"
+	"path/filepath"
 
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 
+	"github.com/ScienceSoft-Inc/integrity-sum/internal/core/services"
 	"github.com/ScienceSoft-Inc/integrity-sum/internal/ffi/bee2"
 	"github.com/ScienceSoft-Inc/integrity-sum/internal/graceful"
 	"github.com/ScienceSoft-Inc/integrity-sum/internal/initialize"
 	"github.com/ScienceSoft-Inc/integrity-sum/internal/logger"
+	"github.com/ScienceSoft-Inc/integrity-sum/internal/repositories"
 )
 
 func main() {
@@ -20,9 +24,26 @@ func main() {
 	// Install logger
 	logger := logger.Init(viper.GetString("verbose"))
 
-	bee2.Bee2HashFile("/home/sshliayonkin@scnsoft.com/go/src/github.com/ScienceSoft-Inc/integrity-sum/go.mod",
-		logger)
-	return
+	// Only for testing bee2 alg. Do not merge
+	// go run ./cmd/k8s-integrity-sum
+	{
+		fn := "./go.sum"
+		p, err := filepath.Abs(fn)
+		if err != nil {
+			logger.Fatalf("filepath.Abs(fn): %v", err)
+		}
+		logger.Infof("- whole file hash : %v: %v", filepath.Base(p), bee2.Bee2HashFile(p, logger))
+
+		// hasher interface
+		repository := repositories.NewAppRepository(logger, new(sql.DB))
+		hs := services.NewHashService(repository, "BEE2", logger)
+		hd, err := hs.CreateHash(fn)
+		if err != nil {
+			logger.Fatalf("CreateHash(): %v", err)
+		}
+		logger.Infof("- hasher interface: %v: %v", hd.FileName, hd.Hash)
+		return
+	}
 
 	// Install migration
 	DBMigration(logger)
